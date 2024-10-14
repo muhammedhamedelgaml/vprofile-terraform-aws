106f70a9cebe0d3d7349e67a168f9e263f851171
muhammedhamedelgaml <muhammedhamedelgaml@gmail.com>
2024-10-14 17:29:39 +0300

    allow 3306 from bastion-host  for initialize DB

--- sec-grp.tf
+++ sec-grp.tf
@@ -89,8 +89,14 @@ resource "aws_security_group" "vprofile-backend-sg" {
     from_port       = 0
     to_port         = 0
     protocol        = "-1"
-    security_groups = [aws_security_group.vprofile-sg]
-
+    security_groups = [aws_security_group.vprofile-sg.id]
+  }
+  ingress {
+    //  allow 3306 from bastion-host security group  for initialize DB
+    from_port       = 3306
+    to_port         = 3306
+    protocol        = "tcp"
+    security_groups = [aws_security_group.vprofile-bastion-sg.id]
   }
 }
 
